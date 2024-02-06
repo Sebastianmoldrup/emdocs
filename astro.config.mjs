b3c783657a1aa43baf236794c2c11a4e3ccc1dbe
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2024-02-06 18:41:37 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Shortcode DOCS',
+      // social: {
+      //   github: 'https://github.com/withastro/starlight'
+      // },
+      sidebar: [
+        {
+          label: 'Shortcodes',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'Lånekalkulator',
+              link: '/guides/lanekalkulator/',
+            },
+          ],
+        },
+        {
+          label: 'Annet',
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+      ],
+    }),
+    tailwind(),
+    react(),
+  ],
+});
---
+++ src/content/docs/reference/example.md
@@ -0,0 +1,8 @@
+---
+title: WIP
+description: WIP
+---
+
+## [Work in progress]
+
+<!-- - Read [about reference](https://diataxis.fr/reference/) in the Diátaxis framework -->
